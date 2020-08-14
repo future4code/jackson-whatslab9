a642e2baa0a4bcc00bdfcd8f04503f62f876734d
brunoCards <bruno.cards27@gmail.com>
2020-08-14 16:32:43 -0300

    Projeto concluído sem os desafios

--- whatslab/src/App.js
+++ whatslab/src/App.js
@@ -1,27 +1,51 @@
-import React from 'react';
-import './App.css';
+import React from "react"
+import "./App.css"
+import styled from "styled-components"
+import { MessageForm } from "./components/MessageForm"
 
-export class Nome extends React.Component {
-  state = {
-    nome: ""
+const AppContainer = styled.div`
+  max-width: 600px;
+  height: 100vh;
+  border: 1px solid black;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  
+`
+const MessagesContainer = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  padding: 20px;
+`
+const BoldText = styled.span`
+  font-weight: bold;
+`
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      messages: [],
+    }
   }
-
-  onChangeName = (event) => {
-    this.setState({valorNome: event.target.value})
+  addMessage = (message) => {
+    this.setState({ messages: [...this.state.messages, message] })
   }
-
   render() {
-    console.log("Nome do remetente: ", this.state.nome)
-
     return (
-      <div>
-        <input
-          value={this.state.valorNome}
-          onChangeName={this.onChangeName}
-          placeholder={"Usuário"}
-        />
-      </div>
+      <AppContainer>
+        <MessagesContainer>
+          {this.state.messages.map((message, index) => (
+            <p key={index} onClick={this.onClick}>
+              <BoldText>{message.user}</BoldText>
+              {": " + message.text}
+            </p>
+          ))}
+        </MessagesContainer>
+        <MessageForm addMessage={this.addMessage} />
+      </AppContainer>
     )
   }
 }
-export default Nome;
+export default App
