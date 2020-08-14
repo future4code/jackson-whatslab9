84688bab83d2c04e26865b53159ca05c5876ca6b
brunoCards <bruno.cards27@gmail.com>
2020-08-14 13:46:56 -0300

    Primeiro checkpoint

--- whatslab/src/App.js
+++ whatslab/src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export class Nome extends React.Component {
+  state = {
+    nome: ""
+  }
 
-export default App;
+  onChangeName = (event) => {
+    this.setState({valorNome: event.target.value})
+  }
+
+  render() {
+    console.log("Nome do remetente: ", this.state.nome)
+
+    return (
+      <div>
+        <input
+          value={this.state.valorNome}
+          onChangeName={this.onChangeName}
+          placeholder={"Usuário"}
+        />
+      </div>
+    )
+  }
+}
+export default Nome;
